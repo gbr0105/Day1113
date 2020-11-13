3a92b68765440e4ce50691ad2d27bb6f374785e8
guobingren <2604545827@qq.com>
2020-11-13 14:53:20 +0800

    //[A]werty

---
+++ src/main/java/com/baidu/pojo/Test.java
@@ -0,0 +1,17 @@
+package com.baidu.pojo;
+
+/**
+ * 创作时间：2020/11/9 12:50
+ * 作者：郭炳仁
+ * 版本：1.0
+ */
+public class Test extends Thread {
+    public static void main(String[] args) {
+        for (int i = 1; i < 101; i++) {
+            System.out.println("wo de main fang fa zhi xing le"+i+"次");
+        }
+    }
+
+
+}
+
